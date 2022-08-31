4d8a94c33f6b0ea247b357d425811b6473049843
Rodrigo Soares <rodrigo.soares0642@outlook.com.br>
2022-08-31 11:42:34 -0300

    testing img in card

--- fronted/src/components/CardsFoods/CardsFoods.tsx
+++ fronted/src/components/CardsFoods/CardsFoods.tsx
@@ -1,14 +1,12 @@
 import { Card } from "./styled";
 
-interface Card {
-  image: string;
-}
-
 export function CardsFoods() {
   return (
     <Card>
       <div className="Card">
-        <div className="Card-image"></div>
+        <div className="Card-image">
+          <img src="https://images.unsplash.com/photo-1504674900247-0877df9cc836?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"/>
+        </div>
         <div className="card-title">
           <h1>Pizza Napoli</h1>
         </div>
